f15e5373fab419bfa40006c3bc22201034e61975
Messias Silva <messiasvedita@hotmail.com>
2022-08-04 16:53:10 -0300

    Injeção de dependencias e inversão de controle com @Component

--- src/main/java/com/senhoresenhora/injesao/notificadores/NotificadorEmail.java
+++ src/main/java/com/senhoresenhora/injesao/notificadores/NotificadorEmail.java
@@ -1,7 +1,6 @@
 package com.senhoresenhora.injesao.notificadores;
 
 import com.senhoresenhora.injesao.clientes.Cliente;
-import org.springframework.stereotype.Component;
 
 //@Component
 public class NotificadorEmail implements Notificador {
